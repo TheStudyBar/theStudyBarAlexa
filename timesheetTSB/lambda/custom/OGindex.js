f5db5b12cb61d250afc2054f655faaf2df1b8be7
AndrewSenol <andrewaes77@gmail.com>
2018-07-30 14:07:34 -0500

    works with RecordTime5

--- timesheetTSB/lambda/custom/OGindex.js
+++ timesheetTSB/lambda/custom/OGindex.js
@@ -1,62 +1,30 @@
-// var Alexa = require('alexa-sdk');
-// var AWS = require('aws-sdk');
+console.log('Loading event');
+var AWS = require('aws-sdk');
+var dynamodb = new AWS.DynamoDB();
 
-// var mysql = require('mysql');
+exports.handler = function(event, context) {
+    console.log("Request received:\n", JSON.stringify(event));
+    console.log("Context received:\n", JSON.stringify(context));
 
-// const APP_ID = 'amzn1.ask.skill.c7ac36f4-d092-4db6-9343-724876bcc9ce';
-
-
-
-// exports.handler = function(event, context, callback){
-//   var alexa = Alexa.handler(event, context);
-//   alexa.appId = APP_ID;
-//   alexa.registerHandlers(handlers);
-//   alexa.execute();
-// };
-
-
-
-// var handlers = {
-//   'LaunchRequest': function () {
-//     this.emit('welcome');
-//   },
-//   'welcome': function() {
-//     this.emit(':ask', 'Would you like to sign in, or check out?');
-//   },
-//   'signIn': function() {
-//     console.log("Starting connection");
-//     var con = mysql.createConnection({
-//         host: 'thestudybarinstance.clstjpeu5zis.us-east-1.rds.amazonaws.com',
-//         user: 'asenol',
-//         password: 'T$Bpassword',
-//         database: 'theStudyBarDB',
-//         table: 'timesheet'
-//       });
-      
-//     con.connect(function(err) {
-//         if (err) throw err;
-//         console.log("Connected!");
-//         var sql = "INSERT INTO timesheet (firstName, lastName) VALUES ('Andrew', 'Senol')";
-//         con.query(sql, function (err, result) {
-//           if (err) throw err;
-//           console.log("1 record inserted");
-//         });
-//       });
-      
-//   },
-//   'Unhandled': function () {
-//     this.emit(':ask','Sorry. I didn\'t understand. Would you like to sign in, or check out');
-//   },
-//   'AMAZON.HelpIntent': function () {
-//     this.emit(':ask','What can I help you with?', 'How can I help?');
-//   },
-//   'AMAZON.CancelIntent': function () {
-//     this.emit(':tell','Okay!');
-//   },
-//   'AMAZON.StopIntent': function () {
-//     this.emit(':tell','Goodbye!');
-//   },
-//   'AMAZON.StartOverIntent': function(){
-//     this.emit('welcome');
-//   }
-// };
+    var tableName = "myinfo";
+    var datetime = new Date().getTime().toString();
+    
+    dynamodb.putItem({
+            "TableName": tableName,
+            "Item": {
+                "name": {
+                    "S": event.name
+                }, 
+                "timedate": {
+                    "N": datetime
+                }
+            }
+        }, function(err, data) {
+            if (err) {
+                context.fail('ERROR: Dynamo failed: ' + err);
+            } else {
+                console.log('Dynamo Success: ' + JSON.stringify(data, null, '  '));
+                context.succeed('SUCCESS');
+            }
+        });
+}
--- timesheetTSB/lambda/custom/index.js
+++ timesheetTSB/lambda/custom/index.js
@@ -1,17 +1,17 @@
 var Alexa = require('alexa-sdk');
 var AWS = require('aws-sdk');
 
-AWS.config.update({region: 'us-east-1'});
+//AWS.config.update({ region: 'us-east-1' });
 
 const APP_ID = 'amzn1.ask.skill.c7ac36f4-d092-4db6-9343-724876bcc9ce';
 
-//var dynamo = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+//var dynamo = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 
 
-exports.handler = function(event, context, callback){
+exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
-    //alexa.dynamoDBTableName = 'RecordTime4';
+    alexa.dynamoDBTableName = 'RecordTime5';
     alexa.appId = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
@@ -21,18 +21,20 @@ var handlers = {
     'LaunchRequest': function () {
         this.emit('welcome');
     },
-    'welcome': function() {
+    'welcome': function () {
         this.emit(':ask', 'Would you like to sign in or check out?');
     },
-    'signIn': function() {
-    
+    'signIn': function () {
+
         var table = 'RecordTime5';
 
         var firstNameSlot = this.event.request.intent.slots.firstName.value;
         var lastNameSlot = this.event.request.intent.slots.lastName.value;
-    
+
         var firstName;
         var lastName;
+
+        var datetime = new Date().getTime().toString();
 
         var dateObj = new Date();
         var month = dateObj.getMonth() + 1; //months from 1-12
@@ -40,86 +42,87 @@ var handlers = {
         var year = dateObj.getFullYear();
         newdate = month + "/" + day + "/" + year;
 
-    
+
         if (firstNameSlot && lastNameSlot) {
             firstName = firstNameSlot;
             lastName = lastNameSlot;
         } else {
             this.emit('ask', 'Sorry, I didn\'t quite get that. Please say: my name is, and then state your first and last name');
         }
-    
+
         if (firstName && lastName) {
             this.attributes['FirstName'] = firstName;
             this.attributes['LastName'] = lastName;
-            this.attributes['userId'] = firstName + lastName;  
-            this.attributes['date'] = newdate;
+            this.attributes['userId'] = firstName + lastName;
+            this.attributes['date'] = datetime;
+            var params = {
+                TableName: 'RecordTime5',
+                Item: {
+                    userId: firstName + lastName,
+                    FirstName: firstName,
+                    LastName: lastName,
+                    date: newdate
+                }
+            };
+    
+            console.log(params);
+    
+            docClient.put(params, function (err, data) {
+                if (err) {
+                    console.error("Error", err);
+                } else {
+                    console.log("Success", data);
+                }
+            });
             this.emit(':ask', `Welcome back ${firstName} ${lastName}. Don't forget to sign out when you leave.`);
-        } else{
+        } else {
             this.emit(':ask', `Sorry I didn\'t quite get that. Sign in by saying "sign in" and then your first and last name`);
         }
-        
-        
-        var params = {
-            TableName: table,
-            Item: {
-                'userId': firstName + lastName,
-                'date': newdate,
-                'FirstName': firstName,
-                'LastName': lastName
-            }
-        };
-
-        console.log(params);
-    
-        docClient.put(params, function(err, data) { 
-            if (err) {
-                console.error("Error", err);
-            } else {
-                console.log("Success", data);
-            }
-        });
-    },
-    'Unhandled': function () {
-        this.emit(':ask','Sorry. I didn\'t understand. Would you like to sign in, or check out');
-    },
-    'AMAZON.HelpIntent': function () {
-        this.emit(':ask','What can I help you with?', 'How can I help?');
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell','Okay!');
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell','Goodbye!');
-    },
-    'AMAZON.StartOverIntent': function(){
-        this.emit('welcome');
-    }
-};
-
-
         // var params = {
         //     TableName: table,
         //     Item: {
-        //         "userID": {
+        //         "userId": {
         //             S: firstName + lastName
-        //         },
-        //         "date": {
-        //             S: Date()
         //         },
         //         "FirstName": {
         //             S: firstName
         //         },
         //         "LastName": {
         //             S: lastName
+        //         },
+        //         "date": {
+        //             S: datetime
         //         }
         //     }
         // };
+
         // console.log(params);
-    
-        // dynamo.putItem(params, function(err, data){
-        //     if (err){
+
+        // dynamo.putItem(params, function (err, data) {
+        //     if (err) {
         //         console.error("Error", err);
-        //     } else{
+        //     } else {
         //         console.log("Success", data.Item);
         //     }
         // });
+        
+
+    
+
+    },
+    'Unhandled': function () {
+        this.emit(':ask', 'Sorry. I didn\'t understand. Would you like to sign in, or check out');
+    },
+    'AMAZON.HelpIntent': function () {
+        this.emit(':ask', 'What can I help you with?', 'How can I help?');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', 'Okay!');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    },
+    'AMAZON.StartOverIntent': function () {
+        this.emit('welcome');
+    }
+};
