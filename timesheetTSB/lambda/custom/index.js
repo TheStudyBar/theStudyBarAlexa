76dce5df3af77e366d1c300fe2c3a64999ebbaff
AndrewSenol <andrewaes77@gmail.com>
2018-07-31 12:30:06 -0500

    removed mysql from timesheet skill

--- timesheetTSB/lambda/custom/index.js
+++ timesheetTSB/lambda/custom/index.js
@@ -11,7 +11,7 @@ var docClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
 exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
-    alexa.dynamoDBTableName = 'RecordTime5';
+    alexa.dynamoDBTableName = 'Timesheet';
     alexa.appId = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
@@ -26,7 +26,7 @@ var handlers = {
     },
     'signIn': function () {
 
-        var table = 'RecordTime';
+        var table = 'Timesheet';
 
         var firstNameSlot = this.event.request.intent.slots.firstName.value;
         var lastNameSlot = this.event.request.intent.slots.lastName.value;
@@ -34,39 +34,21 @@ var handlers = {
         var firstName;
         var lastName;
 
-        // var datetime = new Date().getTime().toString();
-
-        // var dateObj = new Date();
-        // var month = dateObj.getMonth() + 1; //months from 1-12
-        // var day = dateObj.getDate();
-        // var year = dateObj.getFullYear();
-        // newdate = month + "/" + day + "/" + year;
-
-
         var date = new Date();
-
         var hour = date.getHours();
         curHour = hour - 5;
-        
-
         var min = date.getMinutes();
         min = (min < 10 ? "0" : "") + min;
-
         var sec = date.getSeconds();
         sec = (sec < 10 ? "0" : "") + sec;
-
         var year = date.getFullYear();
-
         var month = date.getMonth() + 1;
         month = (month < 10 ? "0" : "") + month;
-
         var day = date.getDate();
         day = (day < 10 ? "0" : "") + day;
 
         currentDate = month + "/" + day + "/" + year;
         currentTime = curHour + ":" + min + ":" + sec;
-
-        
 
 
         if (firstNameSlot && lastNameSlot) {
@@ -83,7 +65,7 @@ var handlers = {
             this.attributes['date'] = currentDate;
             this.attributes['timeIn'] = currentTime;
             var params = {
-                TableName: 'RecordTime',
+                TableName: 'Timesheet',
                 Item: {
                     userId: firstName + lastName,
                     FirstName: firstName,
@@ -102,44 +84,79 @@ var handlers = {
                     console.log("Success", data);
                 }
             });
-            this.emit(':ask', `Welcome back ${firstName} ${lastName}. Don't forget to sign out when you leave.`);
+            this.emit(':tell', `Welcome back ${firstName} ${lastName}. Don't forget to sign out when you leave.`);
         } else {
             this.emit(':ask', `Sorry I didn\'t quite get that. Sign in by saying "sign in" and then your first and last name`);
         }
 
+    },
+    'signOut': function () {
+
+        var table = 'Timesheet';
+
+        var firstNameSlot = this.event.request.intent.slots.firstName.value;
+        var lastNameSlot = this.event.request.intent.slots.lastName.value;
+
+        var firstName;
+        var lastName;
+
+        var date = new Date();
+        var hour = date.getHours();
+        curHour = hour - 5;
+        var min = date.getMinutes();
+        min = (min < 10 ? "0" : "") + min;
+        var sec = date.getSeconds();
+        sec = (sec < 10 ? "0" : "") + sec;
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        month = (month < 10 ? "0" : "") + month;
+        var day = date.getDate();
+        day = (day < 10 ? "0" : "") + day;
+
+        currentDate = month + "/" + day + "/" + year;
+        currentTime = curHour + ":" + min + ":" + sec;
 
 
-        // var params = {
-        //     TableName: table,
-        //     Item: {
-        //         "userId": {
-        //             S: firstName + lastName
-        //         },
-        //         "FirstName": {
-        //             S: firstName
-        //         },
-        //         "LastName": {
-        //             S: lastName
-        //         },
-        //         "date": {
-        //             S: datetime
-        //         }
-        //     }
-        // };
+        if (firstNameSlot && lastNameSlot) {
+            firstName = firstNameSlot;
+            lastName = lastNameSlot;
+        } else {
+            this.emit('ask', 'Sorry, I didn\'t quite get that. Please say: my name is, and then state your first and last name');
+        }
 
-        // console.log(params);
+        if (firstName && lastName) {
+            this.attributes['FirstName'] = firstName;
+            this.attributes['LastName'] = lastName;
+            this.attributes['userId'] = firstName + lastName;
+            this.attributes['date'] = currentDate;
+            this.attributes['timeOut'] = currentTime;
+            var params = {
+                TableName: 'Timesheet',
+                Item: {
+                    userId: firstName + lastName,
+                    FirstName: firstName,
+                    LastName: lastName,
+                    date: currentDate,
+                    timeOut: currentTime
+                }
+            };
 
-        // dynamo.putItem(params, function (err, data) {
-        //     if (err) {
-        //         console.error("Error", err);
-        //     } else {
-        //         console.log("Success", data.Item);
-        //     }
-        // });
+            console.log(params);
 
-
-
-
+            docClient.update(params, function (err, data) {
+                if (err) {
+                    console.error("Error", err);
+                } else {
+                    console.log("Success", data);
+                }
+            });
+            this.emit(':tell', `See you later, ${firstName} ${lastName}. Hope you have a great day!`);
+        } else {
+            this.emit(':ask', `Sorry I didn\'t quite get that. Sign in by saying "sign in" and then your first and last name`);
+        }
+    },
+    'success': function () {
+        this.emit(':ask', `Welcome back!`);
     },
     'Unhandled': function () {
         this.emit(':ask', 'Sorry. I didn\'t understand. Would you like to sign in, or check out');
